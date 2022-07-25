74054fb8b6f53fa99f20d5390c2d730dda737d33
Jiahe <yi.jiahe00@gmail.com>
2022-07-26 00:02:52 +0800

    [WIP] match BusArrival response

--- lib/lta_bus_timings_web/controllers/page_controller.ex
+++ lib/lta_bus_timings_web/controllers/page_controller.ex
@@ -2,8 +2,16 @@ defmodule LtaBusTimingsWeb.PageController do
   use LtaBusTimingsWeb, :controller
 
   def index(conn, _params) do
+    token = Application.fetch_env!(:lta_bus_timings, :api_account_key)
+    url = "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=83139"
+    headers = ["AccountKey": token, "Accept": "Application/json"]
+    {:ok, response} = HTTPoison.get(url, headers)
+
+    %{"Services": services} = Jason.decode!(response.body)
+
     conn
     |> assign(:api_account_key, Application.fetch_env!(:lta_bus_timings, :api_account_key))
+    |> assign(:services, services)
     |> render("index.html")
   end
 end
