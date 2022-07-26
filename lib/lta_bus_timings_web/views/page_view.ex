a3c18de87b7c5cc45527e344f5a7513da3e128cf
Yi Jiahe <yi.jiahe00@gmail.com>
2022-07-26 13:03:45 +0800

    add function to convert arrival time to estimated time in mins & seconds

--- lib/lta_bus_timings_web/views/page_view.ex
+++ lib/lta_bus_timings_web/views/page_view.ex
@@ -1,3 +1,10 @@
 defmodule LtaBusTimingsWeb.PageView do
   use LtaBusTimingsWeb, :view
+
+  def seconds_till_arrival(arrival_time) do
+    {:ok, datetime_arrival, _} = DateTime.from_iso8601(arrival_time)
+    {:ok, datetime_now} = DateTime.now("Etc/UTC")
+    seconds = DateTime.diff(datetime_arrival, datetime_now, :second)
+    "#{div(seconds, 60)}m#{rem(seconds, 60)}s"
+  end
 end
