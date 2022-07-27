c357425e271906708403a1111d515db08d7c3bff
Jiahe <yi.jiahe00@gmail.com>
2022-07-27 21:23:24 +0800

    Sort buses by favourited then arrival time

--- lib/lta_bus_timings_web/controllers/page_controller.ex
+++ lib/lta_bus_timings_web/controllers/page_controller.ex
@@ -2,13 +2,6 @@ defmodule LtaBusTimingsWeb.PageController do
   use LtaBusTimingsWeb, :controller
 
   def index(conn, %{"stop_id" => stop_id}) do
-    services = case LtaBusTimings.LTADataMallWrapper.get_bus_arrivals(stop_id) do
-      %{"Services" => services} ->
-        services
-      _ ->
-        []
-    end
-
     favourites = LtaBusTimingsWeb.Favourites.get_favourites(conn)
     favourite_stops = Map.keys(favourites)
     favourited_services = case favourites[stop_id] do
@@ -17,6 +10,14 @@ defmodule LtaBusTimingsWeb.PageController do
       favourited_services ->
         favourited_services
     end
+
+    services = case LtaBusTimings.LTADataMallWrapper.get_bus_arrivals(stop_id) do
+      %{"Services" => services} ->
+        services
+      _ ->
+        []
+    end
+    services = Enum.sort_by(services, fn e -> IO.inspect{!Enum.member?(favourited_services, e["ServiceNo"]), e["NextBus"]["EstimatedArrival"]} end)
 
     conn
     |> assign(:stop_id, stop_id)
