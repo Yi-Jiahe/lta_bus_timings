19082743d101f7f3914d678df1bd535f85a78c9c
Yi Jiahe <yi.jiahe00@gmail.com>
2022-07-27 16:49:35 +0800

    Populate db with bus stops

--- lib/lta_bus_timings/lta_datamall_wrapper.ex
+++ lib/lta_bus_timings/lta_datamall_wrapper.ex
@@ -1,13 +1,22 @@
 defmodule LtaBusTimings.LTADataMallWrapper do
+  def get_bus_arrivals(bus_stop_code) do
+    url =
+      "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=#{bus_stop_code}"
 
-def get_bus_arrivals(bus_stop_code) do
-    token = Application.fetch_env!(:lta_bus_timings, :api_account_key)
-    url = "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=#{bus_stop_code}"
-    headers = ["AccountKey": token, "Accept": "Application/json"]
+    headers = [AccountKey: Application.fetch_env!(:lta_bus_timings, :api_account_key), Accept: "Application/json"]
     {:ok, response} = HTTPoison.get(url, headers)
 
     {:ok, body} = Jason.decode(response.body)
     body
-end
+  end
 
+  def get_bus_stops() do
+    url = "http://datamall2.mytransport.sg/ltaodataservice/BusStops"
+
+    headers = [AccountKey: Application.fetch_env!(:lta_bus_timings, :api_account_key), Accept: "Application/json"]
+    {:ok, response} = HTTPoison.get(url, headers)
+
+    {:ok, body} = Jason.decode(response.body)
+    body
+  end
 end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,30 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias LtaBusTimings.{Repo, BusStop}
+import Ecto.Query
+
+from(s in BusStop) |> Repo.delete_all()
+
+%{"value" => bus_stops} = LtaBusTimings.LTADataMallWrapper.get_bus_stops()
+
+for %{
+      "BusStopCode" => bus_stop_code,
+      "RoadName" => road_name,
+      "Description" => description,
+      "Latitude" => latitude,
+      "Longitude" => longitude
+    } <- bus_stops do
+  Repo.insert!(%BusStop{
+    bus_stop_code: bus_stop_code,
+    road_name: road_name,
+    description: description,
+    latitude: latitude,
+    longitude: longitude
+  })
+end
+
+n_bus_stops = Repo.one(from s in BusStop, select: count(s.id))
+# Should be ~500 stops
+IO.puts(n_bus_stops)
